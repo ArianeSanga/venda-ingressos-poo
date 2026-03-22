bbb39683d0e7c087958243812829c87927166452
Ariane Minguini Sanga <ariane.sanga@aluno.ifsp.edu.br>
2026-03-22 12:34:14 -0300

    feat(S03): desserializa ingressos ao abrir TelaInicial

--- src/venda_ingresso/ui/TelaInicial.java
+++ src/venda_ingresso/ui/TelaInicial.java
@@ -9,58 +9,59 @@ import java.util.ArrayList;
 import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JPanel;
+import venda_ingresso.entities.Ingresso;
+import venda_ingresso.services.GerenciadorArquivo;
 
 /**
  *
  * @author Junior
  */
-public class TelaInicial extends JDialog {   
-    
+public class TelaInicial extends JDialog {
+
     private JPanel painelFundo;
     private JButton btnComprar;
     private JButton btnGerarRelatorio;
-    
+
     ArrayList<Ingresso> ingressos;
-     
-    GerenciadorIngresso gerenciador = new GerenciadorIngresso();
-   
-    
-    public TelaInicial() {    
-        criarComponentesTela();  
+
+    public TelaInicial() {
+        // R14 - tenta desserializar ingressos ao abrir, se nao existir inicia lista vazia
+        ingressos = (ArrayList<Ingresso>) GerenciadorArquivo.desserializar("ingressos.ser");
+        criarComponentesTela();
     }
-    
+
     public TelaInicial(JanelaCadastroIngresso cadastro, boolean isModal, ArrayList<Ingresso> ingressos) {
-        super(cadastro, isModal);       
+        super(cadastro, isModal);
         this.ingressos = ingressos;
         criarComponentesTela();
-    } 
-    
+    }
+
     private void criarComponentesTela() {
         btnComprar = new JButton("Comprar Ingresso");
         btnGerarRelatorio = new JButton("Gerar Relatório");
-        
-        btnComprar.addActionListener((e) -> {           
-           JanelaCadastroIngresso janelaCadastroIngresso = new JanelaCadastroIngresso();
-           janelaCadastroIngresso.setVisible(true); 
-        });       
-        
-        btnGerarRelatorio.addActionListener((e) -> {           
-           setVisible(false);
-           JanelaGrafica janelaGrafica = new JanelaGrafica();
-           
-           janelaGrafica.setVisible(true); 
-           janelaGrafica.imprimirRelatorio(ingressos);
+
+        btnComprar.addActionListener((e) -> {
+            JanelaCadastroIngresso janelaCadastroIngresso = new JanelaCadastroIngresso();
+            janelaCadastroIngresso.setVisible(true);
         });
-        
+
+        btnGerarRelatorio.addActionListener((e) -> {
+            setVisible(false);
+            JanelaGrafica janelaGrafica = new JanelaGrafica();
+
+            janelaGrafica.setVisible(true);
+            janelaGrafica.imprimirRelatorio(ingressos);
+        });
+
         painelFundo = new JPanel();
         painelFundo.add(btnComprar);
         painelFundo.add(btnGerarRelatorio);
-        
+
         add(painelFundo);
-        setDefaultCloseOperation(DISPOSE_ON_CLOSE);// Só fecha a janela(Esconde). Não fecha a aplicação(EXIT_ON_CLOSE)
+        setDefaultCloseOperation(DISPOSE_ON_CLOSE);
         setLocationRelativeTo(null);
         pack();
         setSize(300, 200);
         setVisible(true);
-     }
+    }
 }
