c3e919ad85bfca9b1177d0cf9919b623e4d45f78
Ariane Minguini Sanga <ariane.sanga@aluno.ifsp.edu.br>
2026-03-22 12:21:57 -0300

    feat(B04): cria QuantidadeInvalidaException e SetorEsgotadoException

---
+++ src/venda_ingresso/exceptions/SetorEsgotadoException.java
@@ -0,0 +1,8 @@
+package venda_ingresso.exceptions;
+
+public class SetorEsgotadoException extends RuntimeException {
+
+    public SetorEsgotadoException(String mensagem) {
+        super(mensagem);
+    }
+}
